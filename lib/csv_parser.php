3616ce84ff6a8e9ddaffbb575327b5466e3b1921
Ibrar Jadoon <ibrar.jadoon90@gmail.com>
2026-04-21 17:31:43 +0500

    feat(csv): normailize header names when reading data from csv

--- lib/csv_parser.php
+++ lib/csv_parser.php
@@ -1,12 +1,17 @@
 <?php
 
+include('str_helpers.php');
+
+/**
+ * Convert CSV records to PHP arrays
+ */
 function csv_to_arr($filename) {
   $handle = fopen($filename, 'r');
   
   $headers = fgetcsv($handle);
-  // remove UTF-8 BOM if exists
-  $headers[0] = preg_replace('/^\xEF\xBB\xBF/', '', $headers[0]);
-  $headers = array_map('trim', $headers);
+
+  $headers[0] = remove_utf8_bom($headers[0]);
+  $headers = array_map('normalize_str', $headers);
   
   $data = [];
 
---
+++ lib/str_helpers.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Normalize string. Remove numbers and special characters. Replace spaces with underscores.
+ */
+function normalize_str($str) {
+  $str = trim($str);
+  $str = strtolower($str);
+  $str = preg_replace('/\s+/', '_', $str); // replace whitespace with single underscore
+  $str = preg_replace('/[^a-z0-9_]/', '', $str);
+  return $str;
+}
+
+/**
+ * Removes UTF-8 BOM characters which may cause problems with PHP parsing.
+ */
+function remove_utf8_bom($str) {
+  $str = preg_replace('/^\xEF\xBB\xBF/', '', $str);
+  return $str;
+}
