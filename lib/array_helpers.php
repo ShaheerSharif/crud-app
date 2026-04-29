c356b994c1f8d0c52838d5dfdb5c5a9c33fc7c81
Shaheer Sharif <sharfshaheer@gmail.com>
2026-04-29 18:17:56 +0500

    refactor(lib): added types to array helpers parameters

--- lib/array_helpers.php
+++ lib/array_helpers.php
@@ -3,7 +3,7 @@
 /**
  * Keeps all key value pairs in array except for excluded keys.
  */
-function filter_all_keys_except($arr, $exclude) {
+function filter_all_keys_except(array $arr, array $exclude) {
   if (empty($exclude)) return $arr;
 
   return array_diff_key($arr, array_flip($exclude));
@@ -12,7 +12,7 @@ function filter_all_keys_except($arr, $exclude) {
 /**
  * Keeps all keys starting with the specified prefix.
  */
-function filter_keys_starting_with($arr, $prefix) {
+function filter_keys_starting_with(array $arr, string $prefix) {
   return array_filter($arr, function ($key) use ($prefix) {
     return strpos($key, $prefix) === 0;
   }, ARRAY_FILTER_USE_KEY);
